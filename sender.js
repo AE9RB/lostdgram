308b5f3b3be7d9d829a86d723abe36bbbb28a6e6
David Turnbull <dturnbull@gmail.com>
2019-01-14 16:12:50 -0800

    remove promise noise

--- sender.js
+++ sender.js
@@ -7,34 +7,27 @@ const GRAMS = 1000;
 const FILL = '##################################################'
 
 function sendgrams() {
-    return new Promise((resolve) => {
-        let pos = 0;
-        const socket = dgram.createSocket('udp4');
-        function funkytown() {
-            if (pos >= GRAMS) {
-                socket.close(() => {
-                    console.log('Closed.');
-                    resolve();
-                });
-                return;
-            }
-            socket.send(`${pos++} ${FILL}${FILL}${FILL}${FILL}${FILL}${FILL}${FILL}`, PORT, (e) => {
-                if (e) {
-                    console.log(e);
-                }
-                if (WOULD_YOU_LIKE_ME_TO_DROP_BUFFERS) {
-                    funkytown();
-                } else {
-                    setTimeout(funkytown, 0);
-                }
+    let pos = 0;
+    const socket = dgram.createSocket('udp4');
+    function funkytown() {
+        if (pos >= GRAMS) {
+            socket.close(() => {
+                console.log('Closed.');
             });
+            return;
         }
-        funkytown();
-    });
+        socket.send(`${pos++} ${FILL}${FILL}${FILL}${FILL}${FILL}${FILL}${FILL}`, PORT, (e) => {
+            if (e) {
+                console.log(e);
+            }
+            if (WOULD_YOU_LIKE_ME_TO_DROP_BUFFERS) {
+                funkytown();
+            } else {
+                setTimeout(funkytown, 0);
+            }
+        });
+    }
+    funkytown();
 }
 
-(async () => {
-    console.log('Sending.')
-    await sendgrams();
-    console.log('Done.')
-})()
+sendgrams();
